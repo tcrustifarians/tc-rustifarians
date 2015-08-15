16f19a1f510b9f1dd526a29931dd98f9c3c01cc6
Steve Tuckner <stevetuckner@gmail.com>
2015-08-15 14:27:40 -0500

    moved game methods to trait

--- steve-guess/src/main.rs
+++ steve-guess/src/main.rs
@@ -8,17 +8,78 @@ enum GameOption {
     GameContinue,
 }
 
+trait GuessingGame {
+    fn new() -> Self;
+
+    fn evaluate_guess(&self, guess: i32, ans: i32) -> GameOption;
+    fn return_guess(&self, prompt:&str) -> Option<i32>;
+    fn answer(&self) -> i32;
+}
+
+struct SimpleGuessingGame;
+
+impl SimpleGuessingGame {}
+
+impl GuessingGame for SimpleGuessingGame {
+    fn new() -> Self {
+        SimpleGuessingGame
+    }
+
+    fn evaluate_guess(&self, guess: i32, ans: i32) -> GameOption
+    {
+        match (guess - ans).abs() {
+            1...2 => {
+                println!("you are hot");
+            },
+            0 => {
+                println!("You are right");
+                return GameDone;
+            },
+            _ => {
+                println!("you are cold");
+            },
+        }
+        
+        GameContinue
+    }
+
+    fn return_guess(&self, prompt:&str) -> Option<i32>
+    {
+      print!("{}", prompt);
+      let _ = std::io::stdout().flush();
+
+      let mut string = String::new();
+
+      std::io::stdin().read_line(&mut string)
+               .ok()
+               .expect("Failed to read line");
+
+      string.trim().parse::<i32>().ok()
+    }
+
+
+    fn answer(&self) -> i32
+    {
+      let num = rand::random::<f32>();
+      let ans: i32 = (num * 10.0).floor() as i32;
+      ans
+    }
+}
+
+
 use GameOption::*;
 
 fn main() {
-    let ans = answer();
+    let game = SimpleGuessingGame::new();
+
+    let ans = game.answer();
 
     println!("Answer: {}", ans);
 
     loop {
-        match return_guess("Enter your guess: ") {
+        match game.return_guess("Enter your guess: ") {
             Some(guess) => {
-                match evaluate_guess(guess, ans) { 
+                match game.evaluate_guess(guess, ans) { 
                     GameDone => {
                         break;
                     },
@@ -32,43 +93,4 @@ fn main() {
     }
 }
 
-fn evaluate_guess(guess: i32, ans: i32) -> GameOption
-{
-    match (guess - ans).abs() {
-        1...2 => {
-            println!("you are hot");
-        },
-        0 => {
-            println!("You are right");
-            return GameDone;
-        },
-        _ => {
-            println!("you are cold");
-        },
-    }
-    
-    GameContinue
-}
-
-fn return_guess(prompt:&str) -> Option<i32>
-{
-  print!("{}", prompt);
-  let _ = std::io::stdout().flush();
-
-  let mut string = String::new();
-
-  std::io::stdin().read_line(&mut string)
-           .ok()
-           .expect("Failed to read line");
-
-  string.trim().parse::<i32>().ok()
-}
-
-
-fn answer() -> i32
-{
-  let num = rand::random::<f32>();
-  let ans: i32 = (num * 10.0).floor() as i32;
-  ans
-}
 
