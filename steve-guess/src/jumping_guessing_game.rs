b3bfdfe3a354d58bc1b031ed427519f36b2209fc
Steve Tuckner <stevetuckner@gmail.com>
2015-09-21 15:56:17 -0500

    renamed jumping guessing game

--- steve-guess/src/jumping_guessing_game.rs
+++ steve-guess/src/jumping_guessing_game.rs
@@ -1,11 +1,11 @@
-use std::io::prelude::*;
-
 extern crate rand;
 
 use guessing_game::GuessingGame;
 use guessing_game::GameOption;
 
-pub struct JumpingGuessingGame;
+pub struct JumpingGuessingGame {
+  answer: i32
+}
 
 impl JumpingGuessingGame {
     pub fn new() -> Self {
