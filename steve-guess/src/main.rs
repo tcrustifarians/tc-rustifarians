ca1b5d97e545731f4bc761ea4060daffe068d047
Steve Tuckner <stevetuckner@gmail.com>
2015-09-21 15:58:13 -0500

    Attempt to add choice for type of game to play

--- steve-guess/src/main.rs
+++ steve-guess/src/main.rs
@@ -3,11 +3,21 @@ mod simple_guessing_game;
 mod guessing_game;
 
 use simple_guessing_game::SimpleGuessingGame;
+use jumping_guessing_game::JumpingGuessingGame;
 use guessing_game::GameOption;
 use guessing_game::GuessingGame;
 
 
 fn main() {
+
+    let response = SimpleGuessingGame::display_prompt_and_get_response("Select game type, 1) Simple, 2) Jumping: ");
+    if response == "1" {
+        let simple_game = SimpleGuessingGame::new();
+    }
+    else {
+        let jumping_game = JumpingGuessingGame::new();
+    }
+
     let game = SimpleGuessingGame::new();
 
     println!("Answer: {}", game.get_answer());
